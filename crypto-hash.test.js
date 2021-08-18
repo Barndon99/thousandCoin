2cda32fcf77d6c4bcf89a19461e6a993c8d0d175
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-18 17:44:39 -0600

    Add sha256 encryption using crpto module

--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -1,10 +1,8 @@
 const cryptoHash = require('./cryptoHash');
 
 describe('cryptoHash()', () => {
-  const hash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8';
-
   it('generates a SHA-256 hash', () => {
-    expect(cryptoHash('password')).toEqual(hash)
+    expect(cryptoHash('password')).toEqual('5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8')
   });
 
   it('produces the same hash with the same arguments regardless of order', () => {
