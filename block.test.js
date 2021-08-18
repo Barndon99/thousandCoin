a56e409086a29b43c9a653c19fc4eda02c90301c
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-18 17:07:20 -0600

    Add testing for genesis (first) block

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const { GENESIS_DATA } = require("./config");
 
 describe("Block Class", () => {
   const timestamp = 'someDate';
@@ -11,11 +12,23 @@ describe("Block Class", () => {
     data,
     hash
   });
+
   //Best practice would be to split these into 4 tests
   it('has a timestamp, lastHash, hash, data', () => {
     expect(block.timestamp).toEqual('someDate');
     expect(block.lastHash).toEqual('someLastHash');
     expect(block.hash).toEqual('someHash');
     expect(block.data.length).toEqual(2);
+  });
+
+  describe('genesis', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+    it('returns the correct data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
   });
 });
---
+++ config.js
@@ -0,0 +1,10 @@
+//GLOBAL VALUES GO HERE
+//Values for the first block, totally arbitrary.
+const GENESIS_DATA = {
+  timestamp: 1,
+  lastHash: '!!!!!',
+  hash: 'firstHash',
+  data: []
+};
+
+module.exports = { GENESIS_DATA }
