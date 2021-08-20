b15289c55612bcea2382f1aa6bf2d2bfd1ccfc6a
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-19 21:27:43 -0600

    Add testing for isValidChain() function

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-  const blockchain = new Blockchain();
+  let blockchain = new Blockchain();
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
 
   it('contains a chain instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true);
@@ -16,4 +20,44 @@ describe('Blockchain', () => {
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
   })
-})
+
+  describe('isValidChain()', () => {
+    describe('when the chain doesn\'t start with genesis', () => {
+      it('returns false', () => {
+        blockchain.chain[0] = {data: 'fake-genesis'};
+
+        expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe('when the chain starts with the genesis block and has multiple blocks', () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: '1'});
+        blockchain.addBlock({ data: '2'});
+        blockchain.addBlock({ data: '3'});
+      })
+      
+      describe('a lastHash reference has changed', () => {
+        it('returns false', () => {
+          blockchain.chain[2].lastHash = '4';
+
+          expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+
+        describe('the chain contains invalid data', () => {
+          it('returns false', () => {
+            blockchain.chain[2].data = 'badData';
+            
+            expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+          });
+        });
+
+        describe('the chain does not contain invalid blocks', () => {
+          it('returns true', () => {            
+            expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+          });
+        });
+      });
+    });
+  });
+});
