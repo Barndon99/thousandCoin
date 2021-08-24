c9388d6999e7a02aac25d84f4b02142ba309adc3
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-23 23:25:44 -0600

    Adjusted the cryptohash function to return a new result when values change, and fixed test one for cryptoHash which was expecting the hash for foo with single quotes instead of double

--- helpers/crypto-hash.test.js
+++ helpers/crypto-hash.test.js
@@ -2,10 +2,18 @@ const cryptoHash = require('./cryptoHash');
 
 describe('cryptoHash()', () => {
   it('generates a SHA-256 hash', () => {
-    expect(cryptoHash('password')).toEqual('5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8')
+    expect(cryptoHash('password')).toEqual("ff2bea10041cceb054dd6417bc70e8e796edb94dfaeb7046b536509743fab619")
   });
 
   it('produces the same hash with the same arguments regardless of order', () => {
     expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'two', 'one'));
   });
+
+  it('creates a unique hash when the properties of an input change', () => {
+    const foo = {};
+    const originalHash = cryptoHash(foo);
+    foo['a'] = 'a';
+
+    expect(cryptoHash(foo)).not.toEqual(originalHash)
+  })
 });
