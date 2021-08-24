8940e3e6d01396395c20be26654370aa4fcdf9b9
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-23 19:21:09 -0600

    Sync the chains on startup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 
 const Blockchain = require('./blockchain.js');
 const PubSub = require('./pubsub.js')
@@ -6,6 +7,9 @@ const PubSub = require('./pubsub.js')
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
@@ -27,7 +31,17 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks');
 })
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body);
+
+      console.log('replace chain on sync with', rootChain);
+      blockchain.replaceChain(rootChain);
+    }
+  });
+};
+
 let PEER_PORT;
 
 //Allows multiple users to connect to the redis network at once
@@ -38,5 +52,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 const port = PEER_PORT || DEFAULT_PORT;
 
 app.listen(port, () => {
-  console.log(`Listening on localhost:${port}`)
+  console.log(`Listening on localhost:${port}`);
+
+  syncChains();
 })
