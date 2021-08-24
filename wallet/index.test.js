535e9fd3fd27ba537748dfc0b50b7efd9b7b6c77
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-23 21:27:52 -0600

    Add tests to verify wallets can sign transactions

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,6 @@
 const Wallet = require('./index');
+const { verifySignature } = require('.')
+
 
 describe('Wallet Class', () => {
   let wallet;
@@ -14,4 +16,28 @@ describe('Wallet Class', () => {
   it('has a public key', () => {
     expect(wallet).toHaveProperty('publicKey');
   })
+
+  describe('it signs data', () => {
+    const data = 'someString';
+    
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign
+        })
+      ).toBe(true);
+    });
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false);
+    });
+  });
 });
