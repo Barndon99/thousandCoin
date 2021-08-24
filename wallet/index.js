03f55d2645ddc63803fa0f775410d7bfd6337fb2
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-23 23:00:41 -0600

    Add new transaction functionality added to the wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const {STARTING_BALANCE} = require('../config');
 const { ec, cryptoHash } = require('../helpers');
+const Transaction = require('./transaction');
 
 
 class Wallet {
@@ -13,6 +14,14 @@ class Wallet {
 
   sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({ recipient, amount }) {
+    if (amount > this.balance) {
+      throw new Error('Insufficient balance');
+    }
+
+    return new Transaction({ senderWallet: this, recipient, amount });
   }
 };
 
