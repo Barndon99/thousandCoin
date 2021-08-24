59d211d7773d897790417861ea5adea88b0302f0
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-24 13:50:30 -0600

    Add skeleton for TransactionMining

---
+++ app/transactionMiner.js
@@ -0,0 +1,21 @@
+class TransactionMiner {
+  constructor() {
+
+  }
+  
+  mineTransactions() {
+    // grab valid transactions from transactionPool
+  
+    // generate rewards for miners
+
+    // add a block consisting of these transactions to the blockchain
+
+    // broadcast the new chain
+
+    // clear all the transactions from the pool once data is collected
+  }
+
+
+}
+
+module.exports = TransactionMiner;
