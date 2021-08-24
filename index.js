397e4eb43754bb69c1b8056422a696acf9a6577b
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-24 12:51:13 -0600

    updated transaction post request

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const request = require('request');
 
 const Blockchain = require('./blockchain/index.js');
-const PubSub = require('./app/pubsub.js')
+const PubSub = require('./app/pubsub.js');
+const TransactionPool = require('./wallet/transactionPool.js');
+const Wallet = require('./wallet')
 
+//starts app
 const app = express();
+//initializes a blockchain instance
 const blockchain = new Blockchain();
+//Initializes a transaction pool across the blockchain using Redis (probably)
+const transactionPool = new TransactionPool();
+//Wallet class is necessary to post transactions
+const wallet = new Wallet();
+//Uses redis to broadcast and recieve updates to the network
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -31,6 +40,28 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks');
 })
 
+app.post('/api/transaction', (req, res) => {
+  const { amount, recipient } = req.body;
+
+  let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey });
+
+  try {
+    if (transaction) {
+      transaction.update({ senderWallet: wallet, recipient, amount });
+    } else {
+      transaction = wallet.createTransaction({ recipient, amount });
+    }
+  } catch(error) {
+    return res.status(400).json({type: 'error', message: error.message });
+  }
+
+  transactionPool.setTransaction(transaction);
+
+  console.log('transactionPool: ', transactionPool);
+
+  res.json({ type: 'success', transaction });
+});
+
 const syncChains = () => {
   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
     if (!error && response.statusCode === 200) {
@@ -49,6 +80,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
+//Set a port allowing for multiple users rather than hardcoding the port
 const port = PEER_PORT || DEFAULT_PORT;
 
 app.listen(port, () => {
